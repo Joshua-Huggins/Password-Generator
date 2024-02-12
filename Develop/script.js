227cfb09e3b4e7c3dd92fef005fa09ee894690c5
Josh Lee <joshua.huggins@sbcglobal.net>
2024-02-11 21:30:58 -0800

    added prompt windows for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var option = ["YES", "NO"]
+
+//Prompt Commands
+var criteria =  function() {
+  //prompts user input on special characters in password
+  var specialchar = window.prompt("Include Special Characters? YES, or NO");
+
+  //prompts user input on including numbers in password
+  var numbers = window.prompt("Include Numbers? Enter YES or NO");
+
+  if (!specialchar) {
+    return;
+  }
+  
+  if (!numbers) {
+    return;
+  }
+}
+
+ criteria();
+
+  specialchar = specialchar.toUpperCase();
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
